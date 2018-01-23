b17a8ae5707654c0745d62ea36cd6e5f86a1346d
asamitanii <g033.9521@gmail.com>
2018-01-23 14:54:10 -0800

    Made the dialog present a DatePicker with a calendar

--- app/src/main/java/com/bignerdranch/android/criminalintent/DatePickerFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/DatePickerFragment.java
@@ -4,6 +4,8 @@ import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
 
 /**
  * Created by tanii_asami on 2018/01/23.
@@ -12,6 +14,8 @@ import android.support.v7.app.AlertDialog;
 public class DatePickerFragment extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        return new AlertDialog.Builder(getActivity()).setTitle(R.string.date_picker_title).setPositiveButton(android.R.string.ok, null).create();
+        View v = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_date, null);
+
+        return new AlertDialog.Builder(getActivity()).setView(v).setTitle(R.string.date_picker_title).setPositiveButton(android.R.string.ok, null).create();
     }
 }
